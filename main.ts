58245ed5342d5b8648451f89ec8c31b2dbc5806b
Abdullah Khan <136432132+DevAbdullah90@users.noreply.github.com>
2025-09-08 13:19:42 +0500

    Add process.exit() to terminate the process after countdown completion.

--- main.ts
+++ main.ts
@@ -1,36 +1,36 @@
-#! /usr/bin/env node  
-
+#! /usr/bin/env node
 
 import inquirer from 'inquirer';
 import chalk from 'chalk';
 
-async function startCountdown(){ 
-const { duration } = await inquirer.prompt({
-    type: "input",
-    name: "duration", 
-    message: "Enter countdown duration (in seconds)",
-    validate: (input)=>{
-        const seconds = parseInt(input);
-        return !isNaN(seconds) && seconds > 0;
-    }
-});
+async function startCountdown() {
+    const { duration } = await inquirer.prompt({
+        type: "input",
+        name: "duration",
+        message: "Enter countdown duration (in seconds)",
+        validate: (input) => {
+            const seconds = parseInt(input);
+            return !isNaN(seconds) && seconds > 0;
+        }
+    });
 
-let remainingTime = parseInt(duration);
+    let remainingTime = parseInt(duration);
 
-let countdownInterval = setInterval(() => {
-    const minutes = Math.floor(remainingTime / 60)
-    const seconds= remainingTime % 60;
-   const formattedTime = `${minutes < 10 ? '0' : ''}${minutes}:${seconds < 10 ? '0' : ''}${seconds}`;
+    let countdownInterval = setInterval(() => {
+        const minutes = Math.floor(remainingTime / 60);
+        const seconds = remainingTime % 60;
+        const formattedTime = `${minutes < 10 ? '0' : ''}${minutes}:${seconds < 10 ? '0' : ''}${seconds}`;
 
-    console.log(chalk.cyan(`${formattedTime}`))
+        console.log(chalk.cyan(`${formattedTime}`));
 
-    if(remainingTime <= 0 ) {
-        clearInterval(countdownInterval);
-        console.log(chalk.green('countdown complete!'));
-    } else {
-        remainingTime--;
-    }
-}, 1000)
+        if (remainingTime <= 0) {
+            clearInterval(countdownInterval);
+            console.log(chalk.green('countdown complete!'));
+            process.exit(); // Explicitly exit the process
+        } else {
+            remainingTime--;
+        }
+    }, 1000);
 };
 
-startCountdown()
+startCountdown();
